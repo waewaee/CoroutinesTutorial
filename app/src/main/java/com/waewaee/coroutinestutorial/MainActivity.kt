694fa7aa8f946b684c3f5913d4cf4819ec6f1ece
Wae Waee <waewaee@gmail.com>
2023-08-23 00:17:11 +0630

    suspend functions done

--- app/src/main/java/com/waewaee/coroutinestutorial/MainActivity.kt
+++ app/src/main/java/com/waewaee/coroutinestutorial/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.waewaee.coroutinestutorial
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,8 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.waewaee.coroutinestutorial.ui.theme.CoroutinesTutorialTheme
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -26,7 +32,24 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        GlobalScope.launch {
+            Log.d(TAG, "Before any task")
+            val networkCallAnswer = doNetworkCall()
+            Log.d(TAG, networkCallAnswer)
+            val networkCallAnswer2 = doNetworkCall2()
+            Log.d(TAG, networkCallAnswer2)
+        }
     }
+        suspend fun doNetworkCall(): String {
+            delay(3000L)
+            return "This is the answer"
+        }
+
+        suspend fun doNetworkCall2(): String {
+            delay(3000L)
+            return "This is the answer2"
+        }
 }
 
 @Composable
