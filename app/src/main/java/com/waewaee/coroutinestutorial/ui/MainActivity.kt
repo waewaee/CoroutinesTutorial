ec609a22b4d6b4da4bd8375f07293993d45a2990
Wae Waee <waewaee@gmail.com>
2023-08-23 22:38:45 +0630

    runBlocking done

--- app/src/main/java/com/waewaee/coroutinestutorial/ui/MainActivity.kt
+++ app/src/main/java/com/waewaee/coroutinestutorial/ui/MainActivity.kt
@@ -1,13 +1,36 @@
 package com.waewaee.coroutinestutorial.ui
 
+import android.util.Log
 import com.waewaee.coroutinestutorial.R
 import com.waewaee.coroutinestutorial.databinding.ActivityMainBinding
 import com.waewaee.coroutinestutorial.ui.base.BaseActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : BaseActivity<ActivityMainBinding>(inflate = ActivityMainBinding::inflate) {
 
+    val TAG = "MainActivity"
+
     override fun initUi() {
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "Before runBlocking")
+        runBlocking {
+            launch(Dispatchers.IO) {
+                delay(3000L)
+                Log.d(TAG, "Finished IO coroutine 1")
+            }
+            launch(Dispatchers.IO) {
+                delay(3000L)
+                Log.d(TAG, "Finished IO coroutine 1")
+            }
+            Log.d(TAG, "Start of runBlocking")
+            delay(5000L)
+            Log.d(TAG, "End of runBlocking")
+        }
+        Log.d(TAG, "After runBlocking")
 
     }
 
