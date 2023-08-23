6fa53282c5be92dc62cd2211c1f97a59d27c3b8f
Wae Waee <waewaee@gmail.com>
2023-08-23 23:25:41 +0630

    job done

--- app/src/main/java/com/waewaee/coroutinestutorial/ui/MainActivity.kt
+++ app/src/main/java/com/waewaee/coroutinestutorial/ui/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.waewaee.coroutinestutorial.ui
 
+import android.util.Log
 import com.waewaee.coroutinestutorial.R
 import com.waewaee.coroutinestutorial.databinding.ActivityMainBinding
 import com.waewaee.coroutinestutorial.ui.base.BaseActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : BaseActivity<ActivityMainBinding>(inflate = ActivityMainBinding::inflate) {
 
+    val TAG = "MainActivity"
+
     override fun initUi() {
         setContentView(R.layout.activity_main)
+
+        val job = GlobalScope.launch(Dispatchers.Default) {
+            repeat(5) {
+                Log.d(TAG, "Coroutine is still working.....")
+                delay(1000L)
+            }
+        }
+        runBlocking {
+            job.join()
+            Log.d(TAG, "Main Thread is continuing.....")
+        }
 
     }
 
