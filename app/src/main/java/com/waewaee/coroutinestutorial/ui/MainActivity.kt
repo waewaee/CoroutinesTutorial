5c0ecdb07b348bdc48d26055b8dac81c9e4deb80
Wae Waee <waewaee@gmail.com>
2023-08-23 23:47:55 +0630

    cancellation done

--- app/src/main/java/com/waewaee/coroutinestutorial/ui/MainActivity.kt
+++ app/src/main/java/com/waewaee/coroutinestutorial/ui/MainActivity.kt
@@ -18,17 +18,24 @@ class MainActivity : BaseActivity<ActivityMainBinding>(inflate = ActivityMainBin
         setContentView(R.layout.activity_main)
 
         val job = GlobalScope.launch(Dispatchers.Default) {
-            repeat(5) {
-                Log.d(TAG, "Coroutine is still working.....")
-                delay(1000L)
+            Log.d(TAG, "Starting long running calculation.....")
+            for(i in 30..40) {
+                Log.d(TAG, "Result for i = $i : ${fib(i)}")
             }
+            Log.d(TAG, "Ending long running calculation.....")
         }
         runBlocking {
             delay(2000L)
             job.cancel()
-            Log.d(TAG, "Main Thread is continuing.....")
+            Log.d(TAG, "Cancelled job!")
         }
 
+    }
+
+    fun fib(n: Int) : Long {
+        return if(n == 0) 0
+        else if(n == 1) 1
+        else fib(n - 1) + fib(n - 2)
     }
 
     override fun observe() {
