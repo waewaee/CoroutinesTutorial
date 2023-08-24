7933de5def3f6a109f7bbc7cdec8fa9716124796
Wae Waee <waewaee@gmail.com>
2023-08-24 16:06:37 +0630

    coroutines with firestore

--- app/src/main/java/com/waewaee/coroutinestutorial/ui/MainActivity.kt
+++ app/src/main/java/com/waewaee/coroutinestutorial/ui/MainActivity.kt
@@ -1,13 +1,40 @@
 package com.waewaee.coroutinestutorial.ui
 
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.waewaee.coroutinestutorial.R
 import com.waewaee.coroutinestutorial.databinding.ActivityMainBinding
 import com.waewaee.coroutinestutorial.ui.base.BaseActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+data class Person(
+    val name: String = "",
+    val age: Int = -1
+)
 
 class MainActivity : BaseActivity<ActivityMainBinding>(inflate = ActivityMainBinding::inflate) {
 
     override fun initUi() {
         setContentView(R.layout.activity_main)
+
+        val tutorialDocument = Firebase.firestore.collection("coroutines")
+            .document("toturial")
+
+        val peter = Person("Peter", 25)
+
+        GlobalScope.launch(Dispatchers.IO) {
+            delay(3000L)
+            tutorialDocument.set(peter).await()
+            val person = tutorialDocument.get().await().toObject(Person::class.java)
+            withContext(Dispatchers.Main) {
+                binding.tvData.text = person.toString()
+            }
+        }
 
     }
 
