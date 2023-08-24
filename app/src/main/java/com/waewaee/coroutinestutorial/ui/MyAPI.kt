c1f13a788b05fbaa9b2ac3897872caa619f88440
Wae Waee <waewaee@gmail.com>
2023-08-24 23:11:08 +0630

    issue fixed

--- app/src/main/java/com/waewaee/coroutinestutorial/ui/MyAPI.kt
+++ app/src/main/java/com/waewaee/coroutinestutorial/ui/MyAPI.kt
@@ -1,10 +1,10 @@
 package com.waewaee.coroutinestutorial.ui
 
-import android.telecom.Call
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface MyAPI {
 
     @GET("/comments")
-    suspend fun getComments(): Call<List<Comment>>
+    suspend fun getComments(): Response<List<Comment>>
 }
