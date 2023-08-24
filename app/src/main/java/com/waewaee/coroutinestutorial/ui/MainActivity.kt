add55c820d78d4db051eb00900b00ae138e05737
Wae Waee <waewaee@gmail.com>
2023-08-24 13:33:19 +0630

    solution

--- app/src/main/java/com/waewaee/coroutinestutorial/ui/MainActivity.kt
+++ app/src/main/java/com/waewaee/coroutinestutorial/ui/MainActivity.kt
@@ -19,8 +19,12 @@ class MainActivity : BaseActivity<ActivityMainBinding>(inflate = ActivityMainBin
 
         GlobalScope.launch(Dispatchers.IO) {
             val time = measureTimeMillis {
-                val answer1 = doNetworkCall1()
-                val answer2 = doNetworkCall2()
+                var answer1: String? = null
+                var answer2: String? = null
+                var job1 = launch { answer1 = doNetworkCall1() }
+                var job2 = launch { answer2 = doNetworkCall2() }
+                job1.join()
+                job2.join()
                 Log.d(TAG, "Answer1 is $answer1")
                 Log.d(TAG, "Answer2 is $answer2")
             }
